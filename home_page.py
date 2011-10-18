0cb298754d8e79d0ae951d7f88f6bdccb95fdb46
Barry Horgan <horgan.barry@gmail.com>
2011-10-18 17:14:24 +1100

    modified sign locator

--- home_page.py
+++ home_page.py
@@ -6,7 +6,7 @@ import base_page
 class HomePage(base_page.BasePage):
 
     _page_title = "Treat Yourself to $200 of Pampering for Just $59! Includes: Facial, Swedish Massage, Eye Treatment & More! - Spreets "
-    _sign_in_link_locator = (by_link_text, "Sign In")
+    _sign_in_link_locator = (By.Text, "Sign In")
     
     def go_to_home_page(self):
         self.selenium.get(self.testsetup.base_url + "/")
