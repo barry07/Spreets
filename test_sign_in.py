f55c6547e9b1e0647245a44c733660e735f40e69
Barry Horgan <horgan.barry@gmail.com>
2011-10-18 16:49:18 +1100

    added test_sign_in.py

---
+++ test_sign_in.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import pytest
+from unittestzero import Assert
+import home_page
+import signin_page
+
+class TestSignIn:
+    def test_user_can_sign_in(self, testsetup):
+        home_pg = home_page.Homepage(testsetup)
+        home_pg.go_to_home_page()
+        home_pg.click_sign_in_link()
+
+        signin_pg = signin_page.SignInPage(testsetup)
+        signin_pg.sign_in()
