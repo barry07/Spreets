c247d016b1ae9b4e03e4c409197076f9a2eb2572
Barry Horgan <horgan.barry@gmail.com>
2011-10-18 17:27:36 +1100

    updated sign in locator

--- home_page.py
+++ home_page.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 
+from selenium.webdriver.common.by import By
+
 import base_page
 
 
 class HomePage(base_page.BasePage):
 
     _page_title = "Treat Yourself to $200 of Pampering for Just $59! Includes: Facial, Swedish Massage, Eye Treatment & More! - Spreets "
-    _sign_in_link_locator = (By.Text, "Sign In")
+    _sign_in_link_locator = (By.LINK_TEXT, "Sign In")
     
     def go_to_home_page(self):
         self.selenium.get(self.testsetup.base_url + "/")
         self.is_the_current_page
 
     def click_sign_in_link(self):
-        self.selenium.find_element_by_link_text(*self._sign_in_link_locator).click()
+        self.selenium.find_element(*self._sign_in_link_locator).click()
         
 
     
