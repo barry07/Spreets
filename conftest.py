860bd80237cccc46dd25bc6be401234b91acc0a1
Barry Horgan <horgan.barry@gmail.com>
2011-10-19 11:17:42 +1100

    minor change

--- conftest.py
+++ conftest.py
@@ -69,7 +69,7 @@ def pytest_addoption(parser):
     parser.addoption("--base-url",
                      action="store",
                      dest="base_url",
-                     default="https://wwwt.spreets.com/",
+                     default="https://wwwt.spreets.com/home/Sydney",
                      help="base URL for the application under test")
 
 
