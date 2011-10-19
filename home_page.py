c242149bbb660ee0bb3cb81d8e58b31dc6e71a68
Barry Horgan <horgan.barry@gmail.com>
2011-10-19 11:17:21 +1100

    minor change

--- home_page.py
+++ home_page.py
@@ -11,8 +11,8 @@ class HomePage(base_page.BasePage):
     _sign_in_link_locator = (By.LINK_TEXT, "Sign In")
     
     def go_to_home_page(self):
-        self.selenium.get(self.testsetup.base_url + "/")
-        self.is_the_current_page
+        self.selenium.get(self.testsetup.base_url)
+        '''self.is_the_current_page'''
 
     def click_sign_in_link(self):
         self.selenium.find_element(*self._sign_in_link_locator).click()
