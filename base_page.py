ca98cab3936c368d81e7b445278724d9c4b400fa
Barry Horgan <horgan.barry@gmail.com>
2011-10-19 11:17:04 +1100

    minor change

--- base_page.py
+++ base_page.py
@@ -10,9 +10,5 @@ class BasePage(Page):
     _page_title_locator = (By.CSS_SELECTOR, "#page-title")
 
     @property
-    def personal_tools_region(self):
-        return personal_tools_region.PersonalToolsRegion(self.testsetup)
-
-    @property
     def page_title(self):
         return self.selenium.find_element(*self._page_title_locator).text
